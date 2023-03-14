8ad97ac96a93cf0958e7e8ea1ad5e27b46b6ec18
Chku <Chku@DESKTOP-KE9P4I2>
2023-03-14 22:54:50 +0400

    Completed the Fifth task.

---
+++ src/FifthTask.java
@@ -0,0 +1,18 @@
+public class FifthTask {
+
+    static int countVariants(int stairsCount){
+        if(stairsCount==1)
+            return 1;
+        if(stairsCount==2)
+            return 2;
+        return countVariants(stairsCount-1)+countVariants(stairsCount-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There is "+countVariants(1)+" variant of climbing the stairs.");
+        System.out.println("There are "+countVariants(2)+" variants of climbing the stairs.");
+        System.out.println("There are "+countVariants(3)+" variants of climbing the stairs.");
+        System.out.println("There are "+countVariants(4)+" variants of climbing the stairs.");
+        System.out.println("There are "+countVariants(5)+" variants of climbing the stairs.");
+    }
+}
