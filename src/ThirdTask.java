12423ddf5a2995d178ea91bd77985fac1007d9e1
Chku <Chku@DESKTOP-KE9P4I2>
2023-03-14 23:49:11 +0400

    Added comments to the Third task.

--- src/ThirdTask.java
+++ src/ThirdTask.java
@@ -1,11 +1,18 @@
 import java.util.Arrays;
 
 public class ThirdTask {
+
+    //find minimum number that's not in the array
     static int notContains (int[] array){
+        //sort array and initialize min variable that equals 1
         Arrays.sort(array);
         int min = 1;
 
+        //iterate through an array
         for(var a:array){
+            //if the min variable is less than given integer in array,
+            //then it's not in the array and should return the answer,
+            //otherwise, we should continue iterating.
             if(a>min){
                 return min;
             }else if(a==min){
