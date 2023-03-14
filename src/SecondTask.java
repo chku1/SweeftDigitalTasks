98436eecb3a0b6e85507c8152c9a079bab3c9245
Chku <Chku@DESKTOP-KE9P4I2>
2023-03-14 23:38:45 +0400

    Added comments to the task.

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -1,11 +1,18 @@
 public class SecondTask {
 
+    //find minimum number of coins needed to reach the given amount
     static int minSplit(int amount){
+        //initialize array of existing coins and ans variable
         int [] arr = {1,5,10,20,50};
         int ans=0;
 
+        //iterate through arr starting from the last element
         for(int i=arr.length-1;i>=0;i--){
+
+            //use the coin of maximum value until possible
+            //to get minimum number of coins
             while(amount-arr[i]>=0){
+                //increase number of coins and decrease the given amount
                 ans++;
                 amount-=arr[i];
             }
