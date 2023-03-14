c54a8fd477d6da1adfdf4f943e98d9fefc87c987
Chku <Chku@DESKTOP-KE9P4I2>
2023-03-14 23:57:59 +0400

    Added comments to the fourth task.

--- src/FourthTask.java
+++ src/FourthTask.java
@@ -1,21 +1,26 @@
 public class FourthTask {
 
+    //function to add two binary Strings
     static String binarySum(String a, String b){
+        //initialize indexes, remains that is carried value and sum for addition.
         int indexOfA=a.length()-1;
         int indexOfB=b.length()-1;
         int remains = 0;
         int sum = 0;
 
+        //created new string for answer
         StringBuilder ans = new StringBuilder();
 
         while(indexOfA>=0 || indexOfB>=0 || remains == 1){
             sum=remains;
 
+            //add both characters
             if(indexOfA>=0)
                 sum+=a.charAt(indexOfA)-'0';
             if(indexOfB>=0)
                 sum+=b.charAt(indexOfB)-'0';
 
+            //calculate remainder and carry value
             ans.append((char)(sum%2+'0'));
             remains=sum/2;
 
